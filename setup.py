0ceff980a9eb96f8e4b17fcc5dd5aab7c50d10e9
Howard <howard.huang@mail.mcgill.ca>
2019-08-13 15:43:41 -0400

    setup prerequisites

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ import setuptools
 setuptools.setup(
     name="tracker-database",
     version="0.1",
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'sqlalchemy',
+        'psycopg2'
+    ]
 )
